5d8629709579cb85308830348e2fd5173da1ee51
Thato <thatofreelance@gmail.com>
2024-09-14 22:26:37 +0200

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Card, Col, Row } from "antd";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className=" w-full p-10" style={{ height: "100vh" }}>
+      <h1 style={{ fontSize: "30px", fontWeight: "bold" }}>Stories</h1>
+      <p>children's Stories</p>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          {" "}
+          <Card title="Card title" bordered={false} style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Col>
+        <Col span={12} />
+
+        <Col span={12} />
+        <Col span={12} />
+      </Row>
+    </div>
+  );
 };
 
 export default HomePage;
