7e87d39e5c5d7fa6b2f09489edbb1e9fe4d5861d
newinstall <thatofreelance@gmail.com>
2024-09-29 15:17:33 +0200

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,20 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Card, Col, Row } from "antd";
+import { Avatar, Card, Col, Row } from "antd";
+import Meta from "antd/es/card/Meta";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className=" w-full p-10" style={{ height: "100vh" }}>
       <h1 style={{ fontSize: "30px", fontWeight: "bold" }}>Stories</h1>
-      <p>children's Stories</p>
+      <p className="mb-10">children's Stories</p>
       <Row gutter={[16, 16]}>
         <Col span={12}>
           {" "}
-          <Card title="Card title" bordered={false} style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
+          <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="story image"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+      // actions={[
+      //   <SettingOutlined key="setting" />,
+      //   <EditOutlined key="edit" />,
+      //   <EllipsisOutlined key="ellipsis" />,
+      // ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="The Lion and the Hare"
+      description="Short storey about a lion who befriends a hare"
+    />
+  </Card>
         </Col>
         <Col span={12} />
 
